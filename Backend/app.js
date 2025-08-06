e901b9cacb00118956341a1b06f589213df19645
Jaydip2001 <jaydip2801@gmail.com>
2025-08-07 01:31:50 +0530

    Add captain registration feature with validation and error handling

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const connectDB = require("./db/db");
 const userRoutes = require('./routes/user.routes');
 const cookiesParser = require("cookie-parser");
+const captainRoutes = require('./routes/captain.routes');
 
 
 connectDB();
@@ -19,5 +20,11 @@ app.use(cookiesParser());
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+
+
+
 module.exports = app;
