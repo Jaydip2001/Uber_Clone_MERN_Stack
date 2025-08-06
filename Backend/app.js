c3dd7f5001405bd89de2c0d2e0420db4b738720b
Jaydip2001 <jaydip2801@gmail.com>
2025-08-07 00:43:44 +0530

    Implement authentication middleware, user profile retrieval, and logout functionality with cookie management

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./db/db");
-const userRoutes = require('./routes/user.routes')
+const userRoutes = require('./routes/user.routes');
+const cookiesParser = require("cookie-parser");
+
 
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
+app.use(cookiesParser());
 
 
 
