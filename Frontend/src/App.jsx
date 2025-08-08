f1ca8189f9543c033e25272653af6aa4da0a72c8
Jaydip2001 <jaydip2801@gmail.com>
2025-08-09 00:57:56 +0530

    feat: update routing and enhance Home page with login link and styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 // import { Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import UserLogin from './pages/UserLogin.jsx'
+import UserSignup from './pages/UserSignup.jsx'
+import Captainlogin from './pages/CaptainLogin.jsx'
+import CaptainSignup from './pages/CaptainSignup.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
-    // <div>
-  {/* <Routes>
+    <div>
+  <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<UserLogin/>} />
+      <Route path='/signup' element={<UserSignup/>} />
+      <Route path='/captain-login' element={<Captainlogin/>} />
+      <Route path='/captain-signup' element={<CaptainSignup/>} />
+      
   </Routes>
-    </div> */}
+    </div>
   )
 }
 
